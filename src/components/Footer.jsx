764cbc5f48a98ef0992e5d30b3d54c31ffdfaf0e
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-13 02:15:37 +0300

    fix: align import paths with exact file casing for production

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <span className="text-muted" style={{ color: 'black' }}>
+            © {new Date().getFullYear()} 7LE8HA Powered by Abdallah Hamdans
+        </span >
+    );
+}
+
+export default Footer   
