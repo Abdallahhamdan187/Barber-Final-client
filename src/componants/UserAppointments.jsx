52c2f28533ff2fbc3c4b66815682ee30485a3f14
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-13 03:31:14 +0300

    feat: build user dashboard view

--- src/componants/UserAppointments.jsx
+++ src/componants/UserAppointments.jsx
@@ -10,7 +10,6 @@ function UserAppointments() {
     const userId = localStorage.getItem("user_id")
 
 
-
     const baseUrl = import.meta.env.VITE_API_URL;
     const [modal, setModal] = useState({
         open: false,
