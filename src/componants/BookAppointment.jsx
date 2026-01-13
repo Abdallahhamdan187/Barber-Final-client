79b15ed495a3908aa14a331863e4977ac793b0ce
Abdallahhamdan187 <getrektplub@github.com~>
2026-01-13 03:32:49 +0300

    feat: implement appointment booking screen

--- src/componants/BookAppointment.jsx
+++ src/componants/BookAppointment.jsx
@@ -9,8 +9,6 @@ const timeSlots = [
     "01:00 PM", "01:30 PM", "02:00 PM", "02:30 PM", "03:00 PM", "03:30 PM", "04:00 PM", "04:30 PM",
     "05:00 PM", "05:30 PM",
 ];
-
-
 function BookAppointment() {
     const navigate = useNavigate();
     const baseUrl = import.meta.env.VITE_API_URL;
